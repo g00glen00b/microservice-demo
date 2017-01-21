9dd942fb3fdd6ba3bbd6ed3f5150f5aac4e6f3c4
g00glen00b <me@g00glen00b.be>
2017-01-21 22:24:41 +0100

    Implemented discovery service

--- discovery-service/src/main/java/be/g00glen00b/service/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/be/g00glen00b/service/DiscoveryServiceApplication.java
@@ -2,11 +2,13 @@ package be.g00glen00b.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
 }
