9b145fe6852b10d5fa783c7ff61da679c5eec21b
g00glen00b <me@g00glen00b.be>
2017-01-21 22:36:26 +0100

    Implemented registering blog service to discovery service

--- blog-service/src/main/java/be/g00glen00b/service/BlogServiceApplication.java
+++ blog-service/src/main/java/be/g00glen00b/service/BlogServiceApplication.java
@@ -2,11 +2,13 @@ package be.g00glen00b.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BlogServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlogServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BlogServiceApplication.class, args);
+    }
 }
