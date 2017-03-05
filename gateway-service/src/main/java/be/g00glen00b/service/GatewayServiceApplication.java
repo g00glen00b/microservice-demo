8a4546dfc8a1dd2c0d0cbffea1679799955a80b3
g00glen00b <me@g00glen00b.be>
2017-03-05 19:33:49 +0100

    Implemented gateway service

--- gateway-service/src/main/java/be/g00glen00b/service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/be/g00glen00b/service/GatewayServiceApplication.java
@@ -2,11 +2,15 @@ package be.g00glen00b.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
 public class GatewayServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
 }
--- profile-service/src/main/java/be/g00glen00b/service/data/ProfileService.java
+++ profile-service/src/main/java/be/g00glen00b/service/data/ProfileService.java
@@ -98,5 +98,5 @@ public class ProfileService {
         } finally {
             IOUtils.closeQuietly(inputStream);
         }
-
+    }
 }
