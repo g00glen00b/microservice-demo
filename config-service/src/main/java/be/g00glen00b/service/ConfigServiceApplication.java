daf13d1a49b7a3d563e178fa1ecb81bfc9925564
g00glen00b <me@g00glen00b.be>
2017-03-05 21:03:29 +0100

    Implemented config service

--- config-service/src/main/java/be/g00glen00b/service/ConfigServiceApplication.java
+++ config-service/src/main/java/be/g00glen00b/service/ConfigServiceApplication.java
@@ -2,11 +2,15 @@ package be.g00glen00b.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
 public class ConfigServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
 }
