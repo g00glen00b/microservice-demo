825420536562f9f906c1a2054a4b015be4915471
g00glen00b <me@g00glen00b.be>
2017-06-17 13:37:36 +0200

    Removed user service (gonna replace it by events between uaa and profile service)

--- user-service/src/main/java/be/g00glen00b/service/ClientChannels.java
+++
@@ -1,9 +0,0 @@
-package be.g00glen00b.service;
-
-import org.springframework.cloud.stream.annotation.Output;
-import org.springframework.messaging.MessageChannel;
-
-public interface ClientChannels {
-    @Output
-    MessageChannel output();
-}
