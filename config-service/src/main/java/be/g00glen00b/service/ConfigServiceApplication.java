aa26c3cbb09396156dfadd6ff0b604863736e259
g00glen00b <me@g00glen00b.be>
2017-06-17 13:38:15 +0200

    Removed Eureka client from config service to make startup exception-free

--- config-service/src/main/java/be/g00glen00b/service/ConfigServiceApplication.java
+++ config-service/src/main/java/be/g00glen00b/service/ConfigServiceApplication.java
@@ -3,10 +3,8 @@ package be.g00glen00b.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableConfigServer
 public class ConfigServiceApplication {
 
