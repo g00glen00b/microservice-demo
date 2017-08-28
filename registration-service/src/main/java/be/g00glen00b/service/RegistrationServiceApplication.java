2f8fc9f05c17ecc7c3c2b6a8621d1c12935fc291
g00glen00b <me@g00glen00b.be>
2017-08-28 23:14:34 +0200

    Fixed binding annotation of registration service

--- registration-service/src/main/java/be/g00glen00b/service/RegistrationServiceApplication.java
+++ registration-service/src/main/java/be/g00glen00b/service/RegistrationServiceApplication.java
@@ -1,11 +1,14 @@
 package be.g00glen00b.service;
 
+import be.g00glen00b.service.model.Registration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableBinding(Registration.class)
 public class RegistrationServiceApplication {
 
     public static void main(String[] args) {
