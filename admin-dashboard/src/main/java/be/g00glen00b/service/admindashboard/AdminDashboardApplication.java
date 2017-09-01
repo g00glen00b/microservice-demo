f0d3fb12793f50a8415c99ce2f6ab41cc6f0ab7f
g00glen00b <me@g00glen00b.be>
2017-09-01 21:19:13 +0200

    Initial implementation admin dashboard

---
+++ admin-dashboard/src/main/java/be/g00glen00b/service/admindashboard/AdminDashboardApplication.java
@@ -0,0 +1,14 @@
+package be.g00glen00b.service.admindashboard;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class AdminDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminDashboardApplication.class, args);
+    }
+}
---
+++ admin-dashboard/src/test/java/be/g00glen00b/service/admindashboard/AdminDashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package be.g00glen00b.service.admindashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdminDashboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
