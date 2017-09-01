102e9797e444087c8ad031795a1e96cd8cc86a93
g00glen00b <me@g00glen00b.be>
2017-09-01 22:30:23 +0200

    Updated Docke compose

--- docker/wrapper.sh
+++ docker/wrapper.sh
@@ -13,7 +13,25 @@ if [ -z ${CONFIG_HOST} ]; then
     echo "No configuration service to connect to, skipping polling"
 else
     while ! exec 6<>/dev/tcp/${CONFIG_HOST}/${CONFIG_PORT}; do
-        echo "Trying to connect to configuration service at ${CONFIG_PRT}..."
+        echo "Trying to connect to configuration service at ${CONFIG_PORT}..."
+        sleep 10
+    done
+fi
+
+if [ -z ${ZOOKEEPER_HOST} ]; then
+    echo "No Zookeeper to connect to, skipping polling"
+else
+    while ! exec 6<>/dev/tcp/${ZOOKEEPER_HOST}/${ZOOKEEPER_PORT}; do
+        echo "Trying to connect to Zookeeper at ${ZOOKEEPER_PORT}..."
+        sleep 10
+    done
+fi
+
+if [ -z ${KAFKA_HOST} ]; then
+    echo "No Kafka to connect to, skipping polling"
+else
+    while ! exec 6<>/dev/tcp/${KAFKA_HOST}/${KAFKA_PORT}; do
+        echo "Trying to connect to Kafka at ${KAFKA_PORT}..."
         sleep 10
     done
 fi
--- install.sh
+++ install.sh
@@ -4,4 +4,6 @@ mvn -f config-service/pom.xml clean package docker:build &&
     mvn -f gateway-service/pom.xml clean package docker:build &&
     mvn -f uaa-service/pom.xml clean package docker:build &&
     mvn -f profile-service/pom.xml clean package docker:build &&
-    mvn -f blog-service/pom.xml clean package docker:build
+    mvn -f blog-service/pom.xml clean package docker:build &&
+    mvn -f registration-service/pom.xml clean package docker:build &&
+    mvn -f admin-dashboard/pom.xml clean package docker:build
