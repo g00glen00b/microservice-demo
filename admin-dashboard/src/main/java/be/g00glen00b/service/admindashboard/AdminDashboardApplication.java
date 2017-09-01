f64665437011a24de90be3baa8538cb9592ab700
g00glen00b <me@g00glen00b.be>
2017-09-01 21:34:24 +0200

    Fixed cloud dependencies version

--- admin-dashboard/src/main/java/be/g00glen00b/service/admindashboard/AdminDashboardApplication.java
+++ admin-dashboard/src/main/java/be/g00glen00b/service/admindashboard/AdminDashboardApplication.java
@@ -3,9 +3,11 @@ package be.g00glen00b.service.admindashboard;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableAdminServer
+@EnableEurekaClient
 public class AdminDashboardApplication {
 
     public static void main(String[] args) {
