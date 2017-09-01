7e29ac666d42203e6bc06bcb27ca63269a342da4
g00glen00b <me@g00glen00b.be>
2017-09-01 22:07:33 +0200

    Removed Spring boot admin client in favour of registration

--- config-service/src/main/java/be/g00glen00b/service/ConfigServiceApplication.java
+++ config-service/src/main/java/be/g00glen00b/service/ConfigServiceApplication.java
@@ -3,9 +3,11 @@ package be.g00glen00b.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServiceApplication {
 
     public static void main(String[] args) {
