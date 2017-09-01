a22d93c97889f830353681106903c5f86b273641
g00glen00b <me@g00glen00b.be>
2017-09-01 23:37:57 +0200

    Using Docker secrets for mail configuration

--- registration-service/src/main/java/be/g00glen00b/service/RegistrationServiceApplication.java
+++ registration-service/src/main/java/be/g00glen00b/service/RegistrationServiceApplication.java
@@ -3,8 +3,11 @@ package be.g00glen00b.service;
 import be.g00glen00b.service.model.Registration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -13,5 +16,11 @@ public class RegistrationServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RegistrationServiceApplication.class, args);
+    }
+
+    @ConditionalOnProperty("mail.configuration")
+    @PropertySource(value = "file:${mail.configuration}", ignoreResourceNotFound = true)
+    public static class RegistrationServiceSecretConfiguration {
+
     }
 }
