51c58caf52c272092f0450f8b2acb022dad04ac9
g00glen00b <me@g00glen00b.be>
2017-09-02 23:15:49 +0200

    Fixed wrapper to use nc in stead of /dev/tcp which is only found on bourne shell

--- docker/wrapper.sh
+++ docker/wrapper.sh
@@ -1,39 +1,48 @@
-#!/bin/bash
+#!/bin/ash
 
 if [ -z ${DATABASE_HOST} ]; then
     echo "No database to connect to, skipping polling"
 else
-    while ! exec 6<>/dev/tcp/${DATABASE_HOST}/${DATABASE_PORT}; do
-        echo "Trying to connect to MySQL at ${DATABASE_PORT}..."
+    echo "Trying to connect to MySQL at ${DATABASE_PORT}..."
+    nc -z -v -w5 ${DATABASE_HOST} ${DATABASE_PORT}
+    while [ $? -ne 0 ]; do
         sleep 10
+        nc -z -v -w5 ${DATABASE_HOST} ${DATABASE_PORT}
     done
 fi
 
 if [ -z ${CONFIG_HOST} ]; then
     echo "No configuration service to connect to, skipping polling"
 else
-    while ! exec 6<>/dev/tcp/${CONFIG_HOST}/${CONFIG_PORT}; do
-        echo "Trying to connect to configuration service at ${CONFIG_PORT}..."
+    echo "Trying to connect to configuration service at ${CONFIG_PORT}..."
+    nc -z -v -w5 ${CONFIG_HOST} ${CONFIG_PORT}
+    while [ $? -ne 0 ]; do
         sleep 10
+        nc -z -v -w5 ${CONFIG_HOST} ${CONFIG_PORT}
     done
 fi
 
 if [ -z ${ZOOKEEPER_HOST} ]; then
     echo "No Zookeeper to connect to, skipping polling"
 else
-    while ! exec 6<>/dev/tcp/${ZOOKEEPER_HOST}/${ZOOKEEPER_PORT}; do
-        echo "Trying to connect to Zookeeper at ${ZOOKEEPER_PORT}..."
+    echo "Trying to connect to Zookeeper at ${ZOOKEEPER_PORT}..."
+    nc -z -v -w5 ${ZOOKEEPER_HOST} ${ZOOKEEPER_PORT}
+    while [ $? -ne 0 ];  do
         sleep 10
+        nc -z -v -w5 ${ZOOKEEPER_HOST} ${ZOOKEEPER_PORT}
     done
 fi
 
 if [ -z ${KAFKA_HOST} ]; then
     echo "No Kafka to connect to, skipping polling"
 else
-    while ! exec 6<>/dev/tcp/${KAFKA_HOST}/${KAFKA_PORT}; do
-        echo "Trying to connect to Kafka at ${KAFKA_PORT}..."
+    echo "Trying to connect to Kafka at ${KAFKA_PORT}..."
+    nc -z -v -w5 ${KAFKA_HOST} ${KAFKA_PORT}
+    while [ $? -ne 0 ]; do
         sleep 10
+        nc -z -v -w5 ${KAFKA_HOST} ${KAFKA_PORT}
     done
 fi
 
-java -Djava.security.egd=file:/dev/./urandom $JAVA_OPTIONS -jar /app.jar
+
+exec java -Djava.security.egd=file:/dev/./urandom $JAVA_OPTIONS -jar /app.jar
